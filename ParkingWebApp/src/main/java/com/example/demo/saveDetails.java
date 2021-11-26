2adf494b3651e6bfc238b9282f5e81ebef9d3d82
Akshat Kumar <akshatkumar.2001@gmail.com>
2021-11-26 13:05:38 +0530

    Update saveDetails.java

--- ParkingWebApp/src/main/java/com/example/demo/saveDetails.java
+++ ParkingWebApp/src/main/java/com/example/demo/saveDetails.java
@@ -6,11 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-
 
 @WebServlet(value="/saveDetails")
 @RequestMapping(value="<%=request.getContextPath()%>/saveDetails", method=RequestMethod.POST)
@@ -22,29 +19,28 @@ public class saveDetails extends HttpServlet {
 		super();
 	}
 
-
 	@RequestMapping(method=RequestMethod.POST)
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-			System.out.println("Begin");
-			String FirstName=request.getParameter("FirstName");
-			String LastName=request.getParameter("LastName");
-			String Username=request.getParameter("Username");
-			String Email=request.getParameter("Email");
-			String Phone=request.getParameter("Phone");
-			String CarReg=request.getParameter("CarReg");
-			String Address=request.getParameter("Address");
-			
-			RegistrationDetails object=new RegistrationDetails();
-			object.setAddress(Address);
-			object.setEmail(Email);
-			object.setFirstName(FirstName);
-			object.setLastName(LastName);
-			object.setPhone(Phone);
-			object.setRegNo(CarReg);
-			object.setUsername(Username);
-			System.out.println("Out");
-			System.out.println(object.getEmail());
+		System.out.println("Begin");
+		String FirstName=request.getParameter("FirstName");
+		String LastName=request.getParameter("LastName");
+		String Username=request.getParameter("Username");
+		String Email=request.getParameter("Email");
+		String Phone=request.getParameter("Phone");
+		String CarReg=request.getParameter("CarReg");
+		String Address=request.getParameter("Address");
+		
+		RegistrationDetails object=new RegistrationDetails();
+		object.setAddress(Address);
+		object.setEmail(Email);
+		object.setFirstName(FirstName);
+		object.setLastName(LastName);
+		object.setPhone(Phone);
+		object.setRegNo(CarReg);
+		object.setUsername(Username);
+		System.out.println("Out");
+		System.out.println(object.getEmail());
 		
 	}
 	
